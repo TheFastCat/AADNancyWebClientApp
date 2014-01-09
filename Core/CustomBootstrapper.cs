6b658a4d3635b27debc4a5b38af4d92326c6c5d6
Reid Peryam <thefastcat@hotmail.com>
2014-01-09 14:51:39 -0700

    Removing unreferenced error handling.

--- Core/CustomBootstrapper.cs
+++ Core/CustomBootstrapper.cs
@@ -4,7 +4,6 @@ using Nancy.Authentication.Stateless;
 using Nancy.Bootstrapper;
 using Nancy.Diagnostics;
 using Nancy.TinyIoc;
-using Nancy.Routing;
 using System;
 
 namespace Core
@@ -14,18 +13,11 @@ namespace Core
     /// </summary>
     public class CustomBootstrapper : DefaultNancyBootstrapper
     {
-
         /// <summary>
         /// This method exists for the purpose of enabling Nancy's Stateless authentication (http://goo.gl/Dtxhve)
         /// </summary>
         protected override void RequestStartup(TinyIoCContainer requestContainer, IPipelines pipelines, NancyContext context)
         {
-
-            pipelines.OnError += (ctx, ex) =>
-            {
-                return null;
-            };
-
             // At request startup we modify the request pipelines to
             // include stateless authentication
             //
